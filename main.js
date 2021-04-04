c70e7eff58530b7e493dee73264c4714bfe2c37f
Andreea Stirbu <stirbu.an@yahoo.com>
2021-04-04 19:58:04 +0100

    background gets color based on clicked image

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 var imgElements = document.querySelectorAll(".img_carousel");
 var textToUpdate = document.querySelector("#text-update");
 
-function updateBackground(color){
-    document.body.style.background = color;
+function updateBackground(elem){
+    let colour;
+    Vibrant.from(elem.src).getPalette(function(err, palette) {
+        console.log(palette);
+        colour = palette["Vibrant"].hex; //DarkVibrant //Muted //Vibrant
+        document.body.style.background = colour;
+    });
 }
 
-imgElements.forEach(elem => {
-    elem.onclick = function() { updateBackground('blue');}
+imgElements.forEach(elem => {      
+    elem.onclick = function() { updateBackground(elem);}
 })
