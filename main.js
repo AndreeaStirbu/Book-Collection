9cca9340fad6bc97c377305dede1e67947520912
Andreea Stirbu <stirbu.an@yahoo.com>
2021-04-05 21:16:15 +0100

    use media queries

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 var imgElements = document.querySelectorAll(".img_slider");
 
-function updateBackground(elem){
+/**
+ * Update the background colour of the page with the vibrant colour of the image that has been clicked.
+ * @param {HTML element} image The image that has been clicked
+ */
+function updateBackgroundColour(image){
     let colour;
-    Vibrant.from(elem.src).getPalette(function(err, palette) {
-        console.log(palette);
+    Vibrant.from(image.src).getPalette(function(err, palette) {
         colour = palette["Vibrant"].hex; //DarkVibrant //Muted //Vibrant
         document.body.style.background = colour;
     });
 }
 
-imgElements.forEach(elem => {      
-    elem.onclick = function() { updateBackground(elem);}
+imgElements.forEach(image => {      
+    image.onclick = function() { updateBackgroundColour(image);}
 })
