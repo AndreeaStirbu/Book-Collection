d3666cc292c01f3a41bd2a1d00b1d4d282f0ff1f
Andreea Stirbu <stirbu.an@yahoo.com>
2021-04-08 00:05:56 +0100

    fix slider broke img on hover

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var imgElements = document.querySelectorAll(".img_slider");
+let btnLeft = document.querySelector(".prev");
+let btnRight = document.querySelector(".next");
 
 /**
  * Update the background colour of the page with the vibrant colour of the image that has been clicked.
@@ -15,3 +17,20 @@ function updateBackgroundColour(image){
 imgElements.forEach(image => {      
     image.onclick = function() { updateBackgroundColour(image);}
 })
+
+btnLeft.addEventListener('click', function() {
+    document.querySelector(".slider_images").scrollLeft -= 100;
+    console.log(document.querySelector(".slider_images").scrollLeft);
+} );
+
+btnRight.addEventListener('click', function() {
+    document.querySelector(".slider_images").scrollLeft += 100;
+    console.log(document.querySelector(".slider_images").scrollLeft);
+} )
+
+// fix on img on hover
+imgElements.forEach(element => {
+    element.addEventListener('mouseover', function(){
+      
+    });
+});
